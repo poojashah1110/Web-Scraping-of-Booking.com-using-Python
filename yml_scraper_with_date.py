3d0295f69ed3f3880a50a4805b3b5ae8c795ab0a
Pooja Shah <72488537+poojashah1110@users.noreply.github.com>
2021-01-21 09:25:42 +0100

    Scraping Codes

---
+++ yml_scraper_with_date.py
@@ -0,0 +1,57 @@
+from selectorlib import Extractor
+import requests 
+from time import sleep
+import csv
+
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('booking_with_date_pooja.yml')
+
+def scrape(url):    
+    headers = {
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1',
+        # You may want to change the user agent if you get blocked
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+        'Referer': 'https://www.booking.com/index.en-gb.html',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8a',
+    }
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Pass the HTML of the page and create 
+    return e.extract(r.text,base_url=url)
+
+# ,encoding="utf-8"
+with open("urls_pooja.txt",'r') as urllist, open('Booking Hotels Monthly Data_Pooja Shah.csv','w',encoding="utf_16") as outfile:
+    fieldnames = [
+        "name",
+        "location",
+        "price",
+        "price_for",
+        "room_type",
+        "beds",
+        "rating",
+        "rating_title",
+        "number_of_ratings",
+        "url",
+        "distance",
+        "start_date",
+        "end_date",
+        "stars",
+        "breakfast_inclusion",
+        "free_cancellation"
+    ]
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+    for url in urllist.readlines():
+        data = scrape(url) 
+        if data:
+            for h in data['hotels']:
+                writer.writerow(h)
+            sleep(2)
